0ddde7d91c1b85d59928df4b56544d15e213c75b
Robin Smets <robinsmets@outlook.de>
2024-09-20 18:00:20 +0200

    added update datastore button to top row

--- BalancedScorecard.PWA/Program.cs
+++ BalancedScorecard.PWA/Program.cs
@@ -1,4 +1,6 @@
 using BalancedScorecard.PWA;
+using BalancedScorecard.PWA.Services;
+using MatBlazor;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 
@@ -8,5 +10,7 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 
 // Register HttpClient for Blazor WebAssembly
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("http://localhost:5555/") });
+builder.Services.AddMatBlazor();
+builder.Services.AddScoped<DataStoreService>();
 
 await builder.Build().RunAsync();
